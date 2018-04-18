088ecdf1c3ef7994582e196539f7176ae139b626
luhuiyue <luhuiyue@weipaidai.com>
2018-04-18 11:11:16 +0800

    luhuiyue

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,4 @@
-// import { loginByUserInfo } from '../../api/login'
+import navJson from '../../../static/json/navlist.json';
 import api from "../../api";
 
 const auth = {
@@ -12,10 +12,20 @@ const auth = {
   },
   actions: {
     getPermission({ commit }){
-        return new Promise((resolve, reject) => {         
-        api.getPermissionList().then((res)=> {
-            commit('setPermissionList',res.data); 
-            resolve(res);
+        return new Promise((resolve, reject) => {
+        const params = {
+          userid:sessionStorage.getItem('USERID')
+        }         
+        api.getAdminRoleByUserid(params).then((res)=> {
+          // console.log(res)
+            let permissionList;
+            if(res.lists[0].r_resource !== undefined){
+              permissionList = JSON.parse(res.lists[0].r_resource);              
+            }else{
+              permissionList = navJson.data;
+            }
+            commit('setPermissionList',permissionList);                   
+            resolve(permissionList);
           });          
         }).catch(error => {
           reject(error);
